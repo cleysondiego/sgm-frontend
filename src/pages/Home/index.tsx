ba4024400e33da4868366cb2d107529eca715654
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-07-29 10:18:47 -0300

    feat: initializes the project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import api from '../../services/api';
+
+import { Title } from './styles';
+
+const Home: React.FC = () => {
+  return <Title>Hello World!</Title>;
+};
+
+export default Home;
