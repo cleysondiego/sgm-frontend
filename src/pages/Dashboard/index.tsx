9c8cc41f45bd6278ef89fa11967e4c5b92e37f8b
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-10-08 23:48:32 -0300

    refactor: removing unused code

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,28 +1,15 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import LoggedHeader from '../../components/LoggedHeader';
-import { useAuth } from '../../hooks/auth';
 
-import { Container, Body, Content } from './styles';
+import { Container, Body } from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
-
-  const handleLogOut = useCallback(() => {
-    signOut();
-  }, [signOut]);
-
   return (
     <Container>
-      <LoggedHeader>
-        <Body>
-          <Content>
-            <h1>Painel de Controle</h1>
-            <button type="button" onClick={handleLogOut}>
-              Sair
-            </button>
-          </Content>
-        </Body>
-      </LoggedHeader>
+      <LoggedHeader />
+      <Body>
+        <p>Cards aqui</p>
+      </Body>
     </Container>
   );
 };
